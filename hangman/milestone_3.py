4f271d5ce523ee5b2b3d814a5e00c12808997759
anaalina1 <ana.haiduc2@gmail.com>
2024-07-26 14:18:17 +0100

    Update milestone_3.py

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -4,9 +4,9 @@ from milestone_2 import word
 def check_guess(guess):
   guess = guess.lower()
   if guess in word:
-  print (f"Good guess! {guess} is in the word.")
-else:
-  print (f"Sorry, {guess} is not in the word. Try again.")
+    print (f"Good guess! {guess} is in the word.")
+  else:
+    print (f"Sorry, {guess} is not in the word. Try again.")
 
 def  ask_for_input():
   while True:
